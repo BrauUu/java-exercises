5b230d806b9f2bd491214b2df9e319ba36e589ba
Brayan <59480526+Brayandotexe@users.noreply.github.com>
2020-03-11 21:29:12 -0300

    Add files via upload

---
+++ Equipe/src/equipe/Equipe.java
@@ -0,0 +1,13 @@
+
+package equipe;
+
+
+public class Equipe {
+
+    public static void main(String[] args) {
+      
+       Jogador jogador = new Jogador()
+        
+    }
+    
+}
---
+++ Equipe/src/equipe/Jogador.java
@@ -0,0 +1,40 @@
+package equipe;
+
+public class Jogador {
+    
+    public static String nomeJ;
+    public String nomeT;
+    public float peso;
+    private int falta;
+    private int gols;
+    
+    public Jogador(){
+        
+    }
+    
+    public int getFalta(){
+        return falta;
+    }
+    
+    private void setFalta(int falta){
+        this.falta = falta;
+    }
+    
+    public int getGols(){
+        return gols;
+    }
+    
+    public void setGols(int gols){
+        this.gols = gols;
+    }
+    
+    public String getficha(){
+        return "NOME: " + nomeJ + "FALTAS: " + falta + "\nGOLS: " + gols + "\nPESO: " + peso;
+    }
+    
+    public void setPeso(float peso){
+        if(peso > this.peso )
+                setFalta(this.falta += falta);
+        this.peso = peso;
+    }
+}
