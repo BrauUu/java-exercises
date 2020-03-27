9b6fd6654d586f801789ab7b4db0f125a304a56c
Brayandotexe <brayanfrank.sm@gmail.com>
2020-03-26 22:16:08 -0300

    heranca

---
+++ Heranca/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Aluno aluno01 = new Aluno("Pedro",21,"155.885.895-54",
+				"Engenharia Ambiental",455);
+		
+		System.out.println(aluno01.getDados());
+
+	}
+
+}
